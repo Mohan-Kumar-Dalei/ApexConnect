0b6d6de22ab3e0307a196edb0569d88edf63625e
Mohan Kumar Dalei <mohankumardalei2001@gmail.com>
2025-10-02 16:03:59 +0530

    backend update

--- src/app.js
+++ src/app.js
@@ -32,7 +32,7 @@ app.use('/api/chats', chatRoute);
 app.use('/api/friends', friendRoute);
 
 app.use(express.static(path.join(__dirname, "client/dist")));
-app.get("*", (req, res) => {
+app.get("/*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client/dist", "index.html"));
 });
 
